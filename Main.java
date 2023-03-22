62f3e7821259d098a841241d61fe214a33290d00
karton675 <127328589+karton675@users.noreply.github.com>
2023-03-22 15:39:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+  
+    String stc = "Hello World";
+    for(int j=0; j<stc.length();j++){
+      System.out.println(stc.charAt(j));
+    }
+    System.out.println("");
+  }
+}
